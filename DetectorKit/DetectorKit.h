287e0037416ec023959b793cd92b380e92e69a2f
Steve Wight <stevendouglaswight@gmail.com>
2017-01-10 08:22:41 -0800

    Initial Commit

---
+++ DetectorKit/DetectorKit.h
@@ -0,0 +1,19 @@
+//
+//  DetectorKit.h
+//  DetectorKit
+//
+//  Created by Steve on 1/10/17.
+//  Copyright © 2017 Steve Wight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DetectorKit.
+FOUNDATION_EXPORT double DetectorKitVersionNumber;
+
+//! Project version string for DetectorKit.
+FOUNDATION_EXPORT const unsigned char DetectorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DetectorKit/PublicHeader.h>
+
+
